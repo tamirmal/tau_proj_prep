350db724f4cecbcfd867478c0f614295fdcb7cfe
Tamir Malka <tamirmal@gmail.com>
2018-12-27 09:40:19 -0800

    upload dataset

--- similar_dataset_to_simple_parser.py
+++ similar_dataset_to_simple_parser.py
@@ -27,7 +27,7 @@ IMG_W = 0
 
 # Classes
 classes = []
-new_classes = []
+new_classes = {}
 
 # format :
 # IMG_20181226_180026.txt:
@@ -35,23 +35,27 @@ new_classes = []
 # 2 0.854375 0.623833 0.225750 0.250333
 # cls x_center y_center w h
 #
-def process_file_single(filename):
-    lst = []
-    imgname = filename.split('.')[:-1] + '.jpg'
-    with open(filename, 'r') as inf:
+def process_file_single(filename, base):
+    entry = {
+        'imgname': None,
+        'data': []
+    }
+
+    boxex = []
+    imgname = filename.split('.')[-2] + '.jpg'
+    entry['imgname'] = imgname
+
+    with open(os.path.join(base, filename), 'r') as inf:
         for line in inf:
             logging.debug("{}".format(line))
 #            cls, x_c, y_c, w, h = line.split()
             data = line.split()
-            lst.append({
-                'imgname': imgname,
-                'data': data,
-            })
+            entry['data'].append(data)
 
-            if data[LIST_ENUMS.CLS] not in classes:
-                classes.append(data[LIST_ENUMS.CLS])
+            if data[LIST_ENUMS.CLS.value] not in classes:
+                classes.append(data[LIST_ENUMS.CLS.value])
 
-    return list
+    return entry
 # End
 
 
@@ -62,10 +66,11 @@ def process_files(dirpath):
         if not f.endswith('.txt'):
             continue
 
-        if not os.path.isfile(str(f.split(',)')[:-1]) + '.jpg'):
-            logging.warning("Cant find image file for text file {}".format(f))
+        img_path = os.path.join(dirpath, str(f.split('.')[-2]) + '.jpg')
+        if not os.path.isfile(img_path):
+            logging.warning("Cant find image file for text file {}".format(img_path))
 
-        img_bboxes = process_file_single(f)
+        img_bboxes = process_file_single(f, dirpath)
         all_bboxes.append(img_bboxes)
 
     return all_bboxes
@@ -82,43 +87,47 @@ def format_for_simple_parser(outf, entries, base_path):
     if os.path.isfile(outf):
         assert 0
 
-    with open(outf, 'w'):
+    class_balance = {val: 0 for k, val in new_classes.items()}
+
+    with open(outf, 'w') as of:
         for entry in entries:
-            X_C, Y_C = entry['data'][LIST_ENUMS.X_C], entry['data'][LIST_ENUMS.Y_C]
-            W, H = entry['data'][LIST_ENUMS.W], entry['data'][LIST_ENUMS.H]
-            CLS = entry['data'][LIST_ENUMS.CLS]
-
-            X_C, Y_C = X_C * IMG_W, Y_C * IMG_H
-            W, H = W * IMG_W, H * IMG_H
-
             im_path = os.path.join(base_path, entry['imgname'])
-            x1, x2 = int(X_C - W/2), int(X_C + W/2)
-            y1, y2 = int(Y_C - H/2), int(Y_C + H/2)
-            cls = new_classes[int(CLS)]
 
-            line = "{},{},{},{},{},{}".format(im_path, x1, y1, x2, y2, cls)
-            logging.debug(line)
-            outf.write(line)
+            for box in entry['data']:
+                X_C, Y_C = box[LIST_ENUMS.X_C.value], box[LIST_ENUMS.Y_C.value]
+                W, H = box[LIST_ENUMS.W.value], box[LIST_ENUMS.H.value]
+                CLS = box[LIST_ENUMS.CLS.value]
 
+                X_C, Y_C = float(X_C) * IMG_W, float(Y_C) * IMG_H
+                W, H = float(W) * IMG_W, float(H) * IMG_H
+
+                x1, x2 = int(X_C - W/2), int(X_C + W/2)
+                y1, y2 = int(Y_C - H/2), int(Y_C + H/2)
+                cls = new_classes[CLS]
+                class_balance[cls] = class_balance[cls] + 1
+
+                line = "{},{},{},{},{},{}\n".format(im_path, x1, y1, x2, y2, cls)
+                logging.debug(line)
+                of.write(line)
+
+    print("outf : {} - class balance = {}".format(outf, str(class_balance)))
     # End
 
 
 def main():
     parser = OptionParser()
-    parser.add_option("-p", "--path", dest="in_path", help="Path to input file.", required=True)
-    parser.add_option("-o", "--out", dest="out_file", help="Path to out file (A default is chosen if not given).", required=True)
-    parser.add_option("-t", "--test", dest="test_part", help="test images ratio", required=True)
-    parser.add_option("-b", "--base", dest="base_path", help="images_base_path", required=True)
+    parser.add_option("-p", "--path", dest="p", help="Path to input file.")
+    parser.add_option("-o", "--out", dest="out_file", help="Path to out file (A default is chosen if not given).")
+    parser.add_option("-t", "--train", dest="t", help="desired number of train images")
+    parser.add_option("-b", "--base", dest="base_path", help="images_base_path")
     (options, args) = parser.parse_args()
-
-    assert (options.t < 0.5)
 
     # Obtain first image dims
     images = os.listdir(options.p)
     for imname in images:
         if imname.endswith('.jpg'):
             global IMG_H, IMG_W
-            IMG_H, IMG_W = cv2.imread(images).shape[:2]
+            IMG_H, IMG_W = cv2.imread(os.path.join(options.p, imname)).shape[:2]
             break
 
     all_img_bbox = process_files(options.p)
@@ -128,23 +137,28 @@ def main():
     global new_classes
     global classes
     if '0' in classes:
-        new_classes = [str(k + 1) for k in classes]
+        new_classes = {k: str(int(k) + 1) for k in classes}
     else:
-        new_classes = [str(k) for k in classes]
+        new_classes = {k: str(int(k)) for k in classes}
 
     # Check output folders
     base_train = os.path.join(options.base_path, 'TRAIN')
     base_test = os.path.join(options.base_path, 'TRAIN')
-
+    """
     if any(os.path.isdir(bb) for bb in [base_train, base_test]):
         print("TRAIN or TEST folders already exists!")
         assert 0
 
     os.mkdir(base_test)
     os.mkdir(base_train)
+    """
 
     # Do work ...
-    test_idx = int(options.t * len(all_img_bbox))
-    format_for_simple_parser(options.out_file + '.train', all_img_bbox[:test_idx])
-    format_for_simple_parser(options.out_file + '.test', all_img_bbox[test_idx:])
+    test_idx = int(options.t)
+    assert (test_idx < len(all_img_bbox))
+    format_for_simple_parser(options.out_file + '.train', all_img_bbox[:test_idx], base_train)
+    format_for_simple_parser(options.out_file + '.test', all_img_bbox[test_idx:], base_test)
 
+
+if __name__ == "__main__":
+    main()
