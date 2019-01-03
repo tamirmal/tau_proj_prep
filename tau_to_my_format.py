7718651d4b5daf9ab7c721d88ad06426c3ed1f7e
Tamir Malka <tamirmal@gmail.com>
2019-01-03 00:50:37 -0800

    new feature

--- tau_to_my_format.py
+++ tau_to_my_format.py
@@ -10,9 +10,13 @@ import cv2
 from enum import Enum
 from random import shuffle
 
-known_classes = []
 
-def format_for_yolo(img_data, base_path='', single_class=True):
+known_classes = []
+one_based_classes = {}
+zero_based_classes = {}
+
+
+def format_for_yolo(img_data, classes_dict, base_path='', single_class=True):
 
     #class_balance = [0] * max(known_classes) # if classes are 1-based there will be a spare element at [0]
 
@@ -25,6 +29,7 @@ def format_for_yolo(img_data, base_path='', single_class=True):
             cls = '0'
         else:
             cls = box['class']
+            cls = classes_dict[cls]
 
 #        class_balance[int(cls)] = class_balance[int(cls)] + 1
 
@@ -75,7 +80,7 @@ def process_file(in_file):
                     })
 
                 if cls not in known_classes:
-                    known_classes.append(int(cls))
+                    known_classes.append(cls)
 
                 counter += 5
 
@@ -85,9 +90,15 @@ def process_file(in_file):
 # End of process_file()
 
 
-def generate_output_file(outf, db, base_path='', format_list=['yolo'], single_class=False):
-
+def generate_output_file(outf, db, zero_based=True, base_path='', format_list=['yolo'], single_class=False):
     suffix_list = []
+    if zero_based:
+        cls_dict = zero_based_classes
+        suffix_list.append('_zero_based')
+    else:
+        cls_dict = one_based_classes
+        suffix_list.append('_one_based')
+
     if single_class:
         suffix_list.append('_single_class')
 
@@ -98,8 +109,17 @@ def generate_output_file(outf, db, base_path='', format_list=['yolo'], single_cl
         with open(out_name, 'w') as of:
             for idx, entry in enumerate(db):
                 if fmt == 'yolo':
-                    line = format_for_yolo(entry, base_path, single_class)
+                    line = format_for_yolo(entry, cls_dict, base_path, single_class)
                     of.write(line)
+
+
+def gen_image_list(outf, db, base_path=''):
+    outf += '.txt'
+    with open(outf, 'w') as of:
+        print("Generating images list {}".format(outf))
+        for img in db:
+            of.write(os.path.join(base_path, img['img_name']) + '\n')
+# End
 
 
 def main():
@@ -119,6 +139,16 @@ def main():
     in_dataset = process_file(options.input)
     print("Number of images in dataset : {}".format(len(in_dataset)))
 
+    global zero_based_classes, one_based_classes
+    if '0' in known_classes or 0 in known_classes:
+        print("Classes/Labels are zero-based. Labels in input file are: {}".format(known_classes))
+        zero_based_classes = {k: k for k in known_classes}
+        one_based_classes = {k: str(int(k)+1) for k in known_classes}
+    else:
+        print("Classes/Labels are one-based. Labels in input file are: {}".format(known_classes))
+        one_based_classes = {k: k for k in known_classes}
+        zero_based_classes = {k: str(int(k) - 1) for k in known_classes}
+
     gen_test = False
     t = int(options.t)
     if t == -1:
@@ -128,15 +158,21 @@ def main():
         gen_test = True
 
     print("Generating Training")
-    generate_output_file(options.out_file + '_train', in_dataset[:t], options.base_path, ['yolo'], single_class=False)
-    generate_output_file(options.out_file + '_train', in_dataset[:t], options.base_path, ['yolo'], single_class=True)
+    generate_output_file(options.out_file + '_train', in_dataset[:t], True, options.base_path, ['yolo'], single_class=False)
+    generate_output_file(options.out_file + '_train', in_dataset[:t], True, options.base_path, ['yolo'], single_class=True)
+    generate_output_file(options.out_file + '_train', in_dataset[:t], False, options.base_path, ['yolo'], single_class=False)
+    generate_output_file(options.out_file + '_train', in_dataset[:t], False, options.base_path, ['yolo'], single_class=True)
+    gen_image_list(options.out_file + '_train_list', in_dataset[:t], options.base_path)
 
     if not gen_test:
         return
 
     print("Generating Testing")
-    generate_output_file(options.out_file + '_test', in_dataset[t:], options.base_path, ['yolo'], single_class=False)
-    generate_output_file(options.out_file + '_test', in_dataset[t:], options.base_path, ['yolo'], single_class=True)
+    generate_output_file(options.out_file + '_test', in_dataset[t:], True, options.base_path, ['yolo'], single_class=False)
+    generate_output_file(options.out_file + '_test', in_dataset[t:], True, options.base_path, ['yolo'], single_class=True)
+    generate_output_file(options.out_file + '_test', in_dataset[t:], False, options.base_path, ['yolo'], single_class=False)
+    generate_output_file(options.out_file + '_test', in_dataset[t:], False, options.base_path, ['yolo'], single_class=True)
+    gen_image_list(options.out_file + '_test_list', in_dataset[t:], options.base_path)
 
 
 if __name__ == "__main__":
